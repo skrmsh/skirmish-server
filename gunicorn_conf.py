4d0079ad94d53311e5d82bcee7f3bada811d3a43
olel <olel@olel.de>
2023-01-20 12:36:16 +0100

    Added gunicorn config

---
+++ gunicorn_conf.py
@@ -0,0 +1,4 @@
+command = 'gunicorn'                                                                                                                │                client.on_receive(data)
+bind = '127.0.0.1:8081'                                                                                                             │
+workers = 1                                                                                                                         │
+raw_env = ["SKIRMSERV_CFG=default.cfg"]
